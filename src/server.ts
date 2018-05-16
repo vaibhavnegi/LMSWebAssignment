b23a15f53e772f4188133e81ca14a8aa8f3a85d2
Vaibhav Negi <vaibhav.negi@nagarro.com>
2018-05-16 16:47:47 +0530

    PackegeJSON

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.get("/", (req: Request, res: Response) => {
     res.sendFile('index.html');
-}); 
+});
 
 const routes = {
     course: courseRoute,
